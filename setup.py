1766de7217341eb7fa5035609821c230452a9594
maolopez <maolopez@users.noreply.github.com>
2025-06-09 14:41:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
-#from setuptools import setup, find_packages
+from setuptools import setup, find_packages
 
-#with open('README.rst', encoding='UTF-8') as f:
-#    readme = f.read()
+with open('README.rst', encoding='UTF-8') as f:
+    readme = f.read()
 
-#setup(
-#    name='ut_anagramma',
-#    version='0.2.0',
-#    description='an internet-facing web service accepting a single word and deriving all possible anagrams.',
-#    long_description=readme,
-#    author='Maurizio Lopez',
-#    author_email='maurizio.school@gmail.com',
-#    packages=find_packages('src'),
-#    package_dir={'': 'src'},
-#    install_requires=['flask'],
-#    entry_points={'console_scripts': ['ut_anagramma=ut_anagramma.main:adder_page',]}
-#)
+setup(
+    name='ut_anagramma',
+    version='0.2.0',
+    description='an internet-facing web service accepting a single word and deriving all possible anagrams.',
+    long_description=readme,
+    author='Maurizio Lopez',
+    author_email='maurizio.school@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=['flask'],
+    entry_points={'console_scripts': ['ut_anagramma=ut_anagramma.main:adder_page',]}
+)
